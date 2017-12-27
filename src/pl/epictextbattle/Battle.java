4aeb7774a323e32d38c6a17bd7253cb40ae68eda
Jaglin <ljaglinski@o2.pl>
2017-12-27 20:41:18 +0100

    Add method is responsible for starting fight between two warriors and check the result of the fight

--- src/pl/epictextbattle/Battle.java
+++ src/pl/epictextbattle/Battle.java
@@ -1,5 +1,20 @@
 package pl.epictextbattle;
 
 public class Battle {
+	
+	//method below starts fight between two warriors
+	public static void startFight(Warrior warrior1, Warrior warrior2) {
+		while(true) {
+			if(getAttackResult(warrior1, warrior2) == "Game Over") {
+				System.out.println("Game Over");
+				break;
+			}
+			if(getAttackResult(warrior2, warrior1) == "Game Over") {
+				System.out.println("Game Over");
+				break;
+			}
+		}
+	}
+	
 
 }
