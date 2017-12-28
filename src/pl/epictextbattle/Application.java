7d6b3d39bda96fcbca8bd45fa99d96c32a1376a2
Jaglin <ljaglinski@o2.pl>
2017-12-28 21:55:02 +0100

    Create two instances of class Warrior

--- src/pl/epictextbattle/Application.java
+++ src/pl/epictextbattle/Application.java
@@ -3,7 +3,8 @@ package pl.epictextbattle;
 public class Application {
 	
 	public static void main(String[] args) {
-		
+		Warrior spartacus = new Warrior("Spartacus", 1000, 200, 50);
+		Warrior commodus = new Warrior("Commodus", 2000, 80, 80);
 	}
 
 }
